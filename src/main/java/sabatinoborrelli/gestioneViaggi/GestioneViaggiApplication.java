6313121c642dc868e98eda210076209e9f366be1
iTino7 <Sabatino.b007@outlook.it>
2025-07-26 10:34:32 +0200

    Initial commit

---
+++ src/main/java/sabatinoborrelli/gestioneViaggi/GestioneViaggiApplication.java
@@ -0,0 +1,13 @@
+package sabatinoborrelli.gestioneViaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneViaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneViaggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sabatinoborrelli/gestioneViaggi/GestioneViaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package sabatinoborrelli.gestioneViaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneViaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
